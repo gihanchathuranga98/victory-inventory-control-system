03298aa91d42d6dea2563b88c382142b903c9147
Nadith Karunarathna <ggnadith2@gmail.com>
2024-07-24 22:21:09 +0530

    prn issue fix

---
+++ victory-inventory-control-system-dev/src/models/dto/Purchase-rn.dto.ts
@@ -0,0 +1,20 @@
+export interface PrnItemDto {
+  prnItemId: string;
+  qty: string;
+  orderedQty: string;
+  estimatedPricePerUnit: string;
+  prnId: string;
+  rmId: string;
+  rmCode?: string;
+  rmName?: string;
+}
+
+export interface PrnDto {
+  id: string;
+  prNo: string;
+  requestedBy: string;
+  remark: string;
+  approvedBy: string;
+  priorityId: number;
+  prnItem: PrnItemDto[];
+}
---
+++ victory-inventory-control-system-dev/src/services/Purchase-rn.service.ts
@@ -0,0 +1,73 @@
+import axios from "axios";
+import {
+  PrnItemResponseDto,
+  PrnResponseDto,
+} from "../models/dto/PrnResponse.dto";
+import { PrnDto } from "../models/dto/Purchase-rn.dto";
+
+export class PrnService {
+  getAllPRNs = async (): Promise<PrnDto[]> => {
+    try {
+      const res: PrnResponseDto[] = (await axios.get("prn/all")).data;
+
+      return res.map((prn) => {
+        return {
+          id: prn.id,
+          prNo: prn.prn_no,
+          requestedBy: prn.requested_by,
+          approvedBy: prn.approved_by,
+          remark: prn.remark,
+          priorityId: prn.priority_id,
+          prnItem: prn.prn_item.map((item: PrnItemResponseDto) => {
+            return {
+              prnItemId: item.id,
+              qty: item.qty,
+              orderedQty: item.ordered_qty,
+              estimatedPricePerUnit: item.estimated_price_per_unit,
+              prnId: item.prn_id,
+              rmId: item.rm_id,
+            };
+          }),
+        };
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+
+  createPrn = async (prn: any) => {
+    try {
+      const res = await axios.post("/prn/add", { ...prn });
+      return res.data;
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+
+  getPrn = async (prnId: string) => {
+    try {
+      const res = await axios.get(`/prn/${prnId}`);
+      return res.data;
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+
+  getPrnItem = async (prnItemId: string) => {
+    try {
+      const res = await axios.get(`/prn/prn-item/${prnItemId}`);
+      return res.data;
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+
+  getNextId = async (): Promise<number> => {
+    try {
+      const res = await axios.get("/prn/next/id");
+      return res.data.newId;
+    } catch (e) {
+      throw e;
+    }
+  };
+}
